b603698cc4643bd992f7c0519af8c5d5720ba04f
faramir07 <97300150+faramir07@users.noreply.github.com>
2022-08-12 19:04:48 -0500

    fix bug

--- api/src/routes/Createdog.js
+++ api/src/routes/Createdog.js
@@ -6,6 +6,11 @@ const router = Router();
 
 router.post('/', async (req, res, next) => {
   const {name, height_min, height_max, weight_min, weight_max, life_min, life_max, img, temperament} = req.body;
+  if(!name || !height_min || !height_max || !weight_min || !weight_max) {
+    return res
+      .status(400)
+      .send({msg: "Falta enviar datos obligatorios"})
+  }
   try{
     const newDog = await postDog(
       name,
